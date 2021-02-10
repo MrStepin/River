9650b6f396e86d895604f729fce152c35aa0ac52
Стёпин Станислав Александрович <stanislavalex@mail.ru>
2021-02-10 18:30:30 +0300

    Revert "first"
    This reverts commit 0fa59efb9ca28666190ef7f6dcb65fc9978d7517.

--- River/Program.cs
+++ River/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace River
@@ -11,41 +10,6 @@ namespace River
     {
         static void Main(string[] args)
         {
-            River river = new River();
-
-            river.CreateFishs();
-
-            int qtyOfPikes = river.listOfPikes.Count;
-            int qtyOfRudds = river.listOfRudds.Count;
-
-            river.StartPositionOfPikes();
-
-            river.StartPositionOfRudds();
-
-            while ((qtyOfPikes > 0) && (qtyOfRudds > 0))
-            {
-                Thread.Sleep(500);
-
-                river.ChangeCoordinatesOfPikes();
-                river.ChangeCoordinatesOfRudds();
-                
-                river.LifeOfPikes();
-                river.LifeOfRudds();
-                river.UpdateFishesInLists();
-
-                qtyOfPikes = river.listOfPikes.Count;
-                qtyOfRudds = river.listOfRudds.Count;
-            }
-
-            if ((qtyOfPikes == 0))
-            {
-                Console.WriteLine("All pikes are died");
-            }
-            else
-            {
-                Console.WriteLine("All rudds are died");
-            }
-            Console.ReadKey();
         }
     }
 }
